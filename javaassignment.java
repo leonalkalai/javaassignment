b69c6aeef7408ede8e1c3852a562973f456da3c0
Leon Kountouras <koundouras@gmail.com>
2022-11-13 09:23:53 +0200

    Update javaassignment.java

--- javaassignment.java
+++ javaassignment.java
@@ -27,6 +27,46 @@ public class Calculations {
 
 // 2) Write a program to check if a even number is palindrome or not - Day 3 Assignment
 
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   
+		System.out.println("please insert a number"); // ask for user to type number
+		  
+		int reverse = 0; // set the reverse number
+		Scanner sc = new Scanner(System.in);
+		int userinput = sc.nextInt(); // the user input number to be checked for palindrome  
+		int temp=userinput;  // store the user number into a temp variable
+		  
+		if (userinput % 2 == 0) { // check if the number is even
+			
+			while(temp > 0) { // check if number is bigger than 0
+				int remainder=temp%10; // get remainder
+				temp=temp/10; // get digits before remainder 
+				reverse=(reverse*10)+remainder; // get reverse number
+			} // end check if number is bigger than 0
+
+ 		   if(reverse==userinput)    { // check if reverse is same with userinput
+ 			   System.out.println(userinput + " is even and palindrome number ");  
+ 		   }
+ 		   
+ 		   else {   
+ 			  System.out.println(userinput + " is even and not a palindrome number ");  
+ 		   }
+		             
+		  } // end // check if the number is even
+		    else { // check if the number is odd
+		            System.out.println(userinput + " is odd"); 
+		  }
+			  
+		 
+		}  
+
+}
+
 
 
 
